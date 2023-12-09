a477a113bac7cf728f05b94c138a98498f0f6fee
sonvendra <sonvendra@gmail.com>
2023-12-09 11:18:02 +0530

    city added

--- src/app/dropdown-filter/dropdown-filter.component.ts
+++ src/app/dropdown-filter/dropdown-filter.component.ts
@@ -3,24 +3,37 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dropdown-filter',
   templateUrl: './dropdown-filter.component.html',
-  styleUrls: ['./dropdown-filter.component.scss']
+  styleUrls: ['./dropdown-filter.component.scss'],
 })
 export class DropdownFilterComponent implements OnInit {
   cities = [
-    {id: 1, name: 'MA, Boston'},
-    {id: 2, name: 'FL, Miami'},
-    {id: 3, name: 'NY, New York', disabled: true},
-    {id: 4, name: 'CA, Los Angeles'},
-    {id: 5, name: 'TX, Dallas'}
-];
-selectedCity:any;
+    {
+      id: '1',
+      name: 'Mumbai',
+      state: 'Maharashtra',
+    },
+    {
+      id: '2',
+      name: 'Delhi',
+      state: 'Delhi',
+    },
+    {
+      id: '3',
+      name: 'Bengaluru',
+      state: 'Karnataka',
+    },
+    {
+      id: '4',
+      name: 'Pune',
+      state: 'Maharashtra',
+    }
+  ];
+  selectedCity: any;
 
-ngOnInit(): void {
-  
-}
- customSearchFn(term: string, item:any) {
-    item.name = item.name.replace(',','');
+  ngOnInit(): void {}
+  customSearchFn(term: string, item: any) {
+    item.name = item.name.replace(',', '');
     term = term.toLocaleLowerCase();
     return item.name.toLocaleLowerCase().indexOf(term) > -1;
-}
+  }
 }
